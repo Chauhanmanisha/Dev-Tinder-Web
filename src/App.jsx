db15ce9ef107f08332914843d355f88b33d0f796
ManishaChauhan2911 <chauhanmani265@gmail.com>
2025-11-24 22:19:42 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+     <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body/>}>
+            <Route path="/login" element={<Login/>}/>
+          </Route>
+          
+        </Routes>
+     </BrowserRouter>
     </>
   );
 }
