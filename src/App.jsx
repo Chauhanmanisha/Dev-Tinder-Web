31266cc41b8729413e0d66b5d8b8011a3f6efd6b
ManishaChauhan2911 <chauhanmani265@gmail.com>
2025-11-30 22:53:34 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./Body";
 import Login from "./Login";
-
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./Feed";
+import Profile from "./Profile";
 
 function App() {
   return (
     <>
-     <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body/>}>
-            <Route path="/login" element={<Login/>}/>
-          </Route>
-          
+     <Provider store={appStore}>
+       <BrowserRouter basename="/">
+          <Routes>
+              <Route path="/" element={<Body/>}>
+                <Route path="/" element={<Feed/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/profile" element={<Profile/>}/>
+             </Route>
         </Routes>
-     </BrowserRouter>
+       </BrowserRouter>
+     </Provider>
     </>
   );
 }
