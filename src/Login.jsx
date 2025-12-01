4e046fb8ef84e8b884d9e638238c7a8d845c76f3
ManishaChauhan2911 <chauhanmani265@gmail.com>
2025-12-01 12:09:15 +0530

    built logout feature

--- src/Login.jsx
+++ src/Login.jsx
@@ -12,6 +12,7 @@ const Login = () => {
     const [password , setPassword] = useState("Laddu@123");
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [error, setError] = useState("")
 
     const handleLogin = async () =>{
         try{
@@ -23,7 +24,7 @@ const Login = () => {
             dispatch(addUser(res.data));
             return navigate("/");
         }catch(err){
-            console.error(err);
+            setError(err?.response?.data || "Something went wrong!");
         }
     }
 
@@ -57,6 +58,7 @@ const Login = () => {
                         />
                     </label>
                 </div>
+                <p className="text bg-red-500">{error}</p>
                 <div className="card-actions justify-center m-2 ">
                 <button className="btn btn-primary" onClick={handleLogin}>Login</button>
                 </div>
